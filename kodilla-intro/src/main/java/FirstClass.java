16718dbf1fd82e07479fa4d9aa0a6504dedb6dec
Wiktoria Staczyk <w7stanczyk@gmail.com>
2022-05-25 11:40:16 +0200

    added FirstClass

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,10 @@
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World"); // [3]
+        System.out.println("Wiktoria"); // [3]
+        System.out.println('S'); // [3]
+        System.out.println(29); // [3]
+
+    }
+}
+
